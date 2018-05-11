353853bbb217112f11d572dbe9f6f6590bb8136a
Dave.Graham <kinkyjalepeno@gmail.com>
2018-05-11 15:26:48 +0100

    Choosing a file and displaying absolute path in Text Field working - Good effort figuring that out.

--- src/MainClasses/MainWindowController.java
+++ src/MainClasses/MainWindowController.java
@@ -1,20 +1,29 @@
 package MainClasses;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
+import java.io.File;
 
 public class MainWindowController {
 
     @FXML
+    private TextField absolutePathLabel;
+
+    @FXML
     private void selectFile(){
 
-        Stage stage = new Stage();
         FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Log Files", "*.log"));
+
         fileChooser.setTitle("Open SMS CDR");
-        fileChooser.showOpenDialog(stage);
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file != null){
+            System.out.println("Selected File: " + file.getAbsolutePath());
+            absolutePathLabel.setText(file.getAbsolutePath());
+        }
 
     }
-
 
 }
